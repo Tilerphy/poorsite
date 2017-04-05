1acd42f4ba07b0e715e3f683e5e1e64b6333a355
Zonas Sun <346410840@163.com>
2017-04-05 17:33:23 +0800

    Update talk.js

--- talk.js
+++ talk.js
@@ -4,6 +4,7 @@ var http = require("http").Server(app);
 var io= require("socket.io")(http);
 var site = "";
 var musicSite="";
+var htmlEncode = require('htmlencode').htmlEncode;
 
 io.on("connection", (socket)=>{
 
@@ -11,16 +12,16 @@ io.on("connection", (socket)=>{
 		if(msg.indexOf("site:") ==0){
 			site= msg.substring(5);
 		}else if(msg.indexOf("img:") == 0){
-			io.emit("talk", "<img src='"+msg.substring(4)+"'/>");
+			io.emit("talk", "<img src='"+htmlEncode(msg.substring(4))+"'/>");
 		}else if(msg.indexOf("music:") ==0){
 			musicSite = msg.substring(6);
 		}else if(msg.indexOf("color:") == 0){
 			var tmp = msg.substring(6);
 			var sp = tmp.split(":");
-			io.emit("talk", "<p style='color:"+sp[0]+"'>"+sp[1]+"</p>");
+			io.emit("talk", "<p style='color:"+sp[0]+"'>"+htmlEncode(sp[1])+"</p>");
 		}
 		else{
-			io.emit("talk", msg);
+			io.emit("talk", htmlEncode(msg));
 		}
 	});
 
